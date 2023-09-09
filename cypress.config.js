9af9b3827c17299779e4e7a67645763f7a63a2f2
Abthakur1 <absk.cts@gmail.com>
2023-09-09 21:31:36 +0530

    update cypress.config.js file

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,27 @@
+const cypress = require("cypress");
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+    }
+    
   },
+
+  env:{
+    
+    environment: "qa",
+    qa:"https://rahulshettyacademy.com/seleniumPractise/#/",
+    uat:"https://www.rahulshettyacademy.com/AutomationPractice/",
+    dev:"dev env url",
+    baseUrl:'https://jsonplaceholder.typicode.com'
+  },
+
+  pageLoadTimeout:50000,
+  defaultCommandTimeout:10000,
+  retries:1,
+  screenshotsFolder:"cypress/screenshots",
+  screenshotOnRunFailure: true
+
 });
